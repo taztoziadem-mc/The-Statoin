f78d584eb916ea5a6c5d92646e29b4ed7d063f79
taztoziadem-mc <taztoziadem@gmail.com>
2026-04-20 09:38:05 -0700

    Add conf.lua for configuration settings

---
+++ conf.lua
@@ -0,0 +1,11 @@
+-- Configuration file
+
+local config = {
+    screenWidth = 800,
+    screenHeight = 600,
+    fullscreen = false,
+    vsync = true,
+    title = "The Forgotten Station"
+}
+
+return config
